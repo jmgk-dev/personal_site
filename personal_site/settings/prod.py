77f5974261a653c707c54faac53e4453d69ff767
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2025-04-08 11:23:22 +0100

    added bugsnag, accessibilty edits

--- personal_site/settings/prod.py
+++ personal_site/settings/prod.py
@@ -10,8 +10,7 @@ ALLOWED_HOSTS = ['jmgk.dev', 'www.jmgk.dev', '134.199.233.167']
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # Bugsnag
-
-# BUGSNAG = {
-#     'api_key': 'e67ab4514492f52ffdc0c60270c6af28',
-#     'project_root': '/home/jamiek/kinorg',
-# }
+BUGSNAG = {
+    'api_key': BUGSNAG_API_KEY,
+    'project_root': '/path/to/your/app',
+}
