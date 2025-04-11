98c204ca760134ca5b9a8670a4bd120497da07f0
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2025-04-11 12:19:11 +0100

    updated fallback method

--- personal_site/settings/dev.py
+++ personal_site/settings/dev.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date, timedelta
+from django.core.management.utils import get_random_secret_key
 
 from .base import *
 
@@ -10,9 +11,6 @@ ALLOWED_HOSTS = ["*"]
 if os.environ.get('RUN_MAIN') != 'true':
     print("Using development settings")
 
-SECRET_KEY = os.getenv(
-    'DJANGO_DEV_KEY',  
-    'django-insecure-default-key-for-dev-only'
-)
+SECRET_KEY = os.getenv('DJANGO_DEV_KEY', get_random_secret_key())
 
 
